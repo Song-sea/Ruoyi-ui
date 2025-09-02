8a93b8af21489b48d069f43354adad6ea57a64b2
Life <Life@qq.com>
2025-09-02 17:53:09 +0800

    股票行情day02

---
+++ ruoyi-modules/ruoyi-modules-stock/src/main/java/com/ruoyi/stock/RuoYiStockApplication.java
@@ -0,0 +1,32 @@
+package com.ruoyi.stock;
+
+import com.ruoyi.common.security.annotation.EnableCustomConfig;
+import com.ruoyi.common.security.annotation.EnableRyFeignClients;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 系统模块
+ * 
+ * @author ruoyi
+ */
+@EnableCustomConfig
+@EnableRyFeignClients
+@SpringBootApplication
+public class RuoYiStockApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(RuoYiStockApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  股票行情模块启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
+    }
+}
